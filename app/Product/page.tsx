55df9821c4dd07823ea263a16e9b3c2c93713e84
abhay <abhay.singh@tothenew.comgit config --global user.name abhaygit config --global user.email abhay.singh@tothenew.com>
2025-02-24 21:00:54 +0530

    fix:productDetails Styling

--- app/Product/page.tsx
+++ app/Product/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import useSWR from "swr";
+import styles from "./Products.module.css";
+import Link from "next/link";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -11,20 +13,28 @@ export default function Product() {
   }
 
   return (
-    <div>
-      <h1>PRODUCTS</h1>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>PRODUCTS</h1>
       <div>
-        {data?.products.map((item: { title: string; description: string; price: number; rating: number; stock: number }, idx: number) => {
-          return (
-            <div key={idx}>
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
-              <p>{item.price}</p>
-              <p>{item.rating}</p>
-              <p>{item.stock}</p>
-            </div>
-          );
-        })}
+        {data?.products.map(
+          (
+            item: { title: string; description: string; price: number; rating: number; stock: number },
+            idx: number
+          ) => {
+            return (
+              <div key={idx} className={styles.card}>
+                <h2 className={styles.title}>{item.title}</h2>
+                <p className={styles.description}>{item.description}</p>
+                <p className={styles.price}>Price: ${item.price}</p>
+                <p className={styles.rating}>Rating: {item.rating}</p>
+                <p className={styles.stock}>Stock: {item.stock}</p>
+                <Link href = {`/Product/${idx+1}`} className={styles.link}>
+                READ MORE
+                </Link>
+              </div>
+            );
+          }
+        )}
       </div>
     </div>
   );
